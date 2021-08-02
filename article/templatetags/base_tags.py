0d39c5dd4940e009f66249e68745582abc659162
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-08-02 12:36:17 +0430

    init template tags article app

--- article/templatetags/base_tags.py
+++ article/templatetags/base_tags.py
@@ -1,5 +1,5 @@
 from django import template
-
+from article.forms import newsletterform
 register=template.Library()
 
 
@@ -9,4 +9,19 @@ def pagination(pages,link,page_range):
         "pages":pages,
         "link":link,
         "page_range":page_range,
+    }
+
+@register.inclusion_tag("article/partials/articles.html")
+def articles(articles,request):
+    return{
+        "articles":articles,
+        "request":request,
+    }
+
+@register.inclusion_tag("article/partials/newsletterform.html")
+def newsletter(request):
+    form=newsletterform()
+    return{
+        "form":form,
+        "request":request,
     }
