c938080cde46032cc6ecbc3978ff10fb3abb54db
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-08-02 12:24:25 +0430

    add urls of resetpassword and setting

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
-from .views import account_profile,Signup,Login,home,logout_view,profile,acc_detail_view,edit_user_detail
+from .forms import emailvalidatepasswordreset
+from .views import (
+setting,
+account_profile,
+Signup,
+Login,
+home,
+logout_view,
+profile,
+acc_detail_view,
+passwordreset,
+passwordresetdone,
+passwordresetcomplete,
+passwordresetconfrim,
+)
 
 app_name="user"
 urlpatterns=[
@@ -9,5 +23,9 @@ urlpatterns=[
     path("logout/",logout_view,name="logout"),
     path("acc_detail/",acc_detail_view,name="acc_detail"),
     path("profile/<str:username>",account_profile,name="account_profile"),
-    path("detail/",edit_user_detail.as_view(),name="user_detail"),
+    path("setting/",setting,name="user_setting"),
+    path("reset_password/",passwordreset.as_view(),name="reset_password"),
+    path("reset/<uidb64>/<token>/",passwordresetconfrim.as_view(),name="reset_password_confrim"),
+    path("reset/done/",passwordresetcomplete.as_view(),name="reset_password_complete"),
+    path("reset_password/done/",passwordresetdone.as_view(),name="reset_password_done"),
 ]
