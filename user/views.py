8aec7b01731c2a15382fd438d5d9602f18b041d6
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-08-02 12:22:08 +0430

    add password reset and change view

--- user/views.py
+++ user/views.py
@@ -1,15 +1,26 @@
 from django.contrib import auth
 from django.contrib.auth import logout
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls.base import reverse
 from .models import User,account_detail
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, request
-from .forms import signupfrom,loginform,acc_detail_form,user_detail_form
-from django.contrib.auth.views import LoginView
+from .forms import signupfrom,loginform,acc_detail_form,user_detail_form,change_password,emailvalidatepasswordreset,notifform
+from django.contrib.auth.views import (
+    LoginView,
+    PasswordResetView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetCompleteView,
+)
+
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 
@@ -81,15 +92,104 @@ def acc_detail_view(request):
     context={"form":form}
     return render(request,"user/account/acc_detail.html",context=context)
 
-class edit_user_detail(LoginRequiredMixin,UpdateView):
-    model=User
-    template_name="user/account/user_detail.html"
-    form_class=user_detail_form
-    success_url=reverse_lazy("user:user_detail")
-    def get_object(self):
-        return User.objects.get(pk=self.request.user.pk)
+# class edit_user_detail(LoginRequiredMixin,UpdateView):
+#     model=User
+#     template_name="user/account/user_detail.html"
+#     form_class=user_detail_form
+#     success_url=reverse_lazy("user:user_detail")
+#     def get_object(self):
+#         return User.objects.get(pk=self.request.user.pk)
 
 def account_profile(request,username):
     user=get_object_or_404(User,username=username)
     context={"pk_user":user}
     return render(request,"user/account/profile.html",context=context)
+
+@login_required
+def setting(request):
+    if request.method=="POST":
+        acc_detail=account_detail.objects.filter(player=request.user).first()
+        if  "update_profile" in  request.POST:
+            form_user_detail=user_detail_form(request.POST,instance=request.user)
+            if form_user_detail.is_valid():
+                form_user_detail.save()
+            if acc_detail:
+                form_account_detail=acc_detail_form(instance=acc_detail)
+            else:
+                form_account_detail=acc_detail_form()
+            form_change_pass=change_password(request.user)
+            form_notif=notifform(instance=request.user)
+        elif "update_account" in request.POST:
+            if acc_detail:
+                form_account_detail=acc_detail_form(request.POST,instance=acc_detail)
+                if form_account_detail.is_valid():
+                    acc_detail=form_account_detail.save(commit=False)
+                    acc_detail.player=request.user
+                    acc_detail.save()
+
+
+            else:
+                form_account_detail=acc_detail_form(request.POST)
+                if form_account_detail.is_valid():
+                    acc_detail=form_account_detail.save(commit=False)
+                    acc_detail.player=request.user
+                    acc_detail.save()
+            form_user_detail=user_detail_form(instance=request.user)
+            form_change_pass=change_password(request.user)
+            form_notif=notifform(instance=request.user)
+        elif "update_password" in request.POST:
+            form_change_pass=change_password(request.user,request.POST)
+            if form_change_pass.is_valid():
+                form_change_pass.save()
+                messages.success(request,"پسورد شما با موفقیت تغییر کرد")
+            if acc_detail:
+                form_account_detail=acc_detail_form(instance=acc_detail)
+            else:
+                form_account_detail=acc_detail_form()
+            form_user_detail=user_detail_form(instance=request.user)
+            form_notif=notifform(instance=request.user)
+        elif "update_notif" in request.POST:
+            form_notif=notifform(request.POST,instance=request.user)
+            if form_notif.is_valid():
+                form_notif.save()
+                messages.success(request,"تنظیمات اطلاعیه ها بروزرسانی شد")
+            form_user_detail=user_detail_form(instance=request.user)
+            if acc_detail:
+                form_account_detail=acc_detail_form(instance=acc_detail)
+            else:
+                form_account_detail=acc_detail_form()
+            form_change_pass=change_password(request.user)
+            
+            
+    else:
+        acc_detail=account_detail.objects.filter(player=request.user).first()
+        if acc_detail:
+            form_account_detail=acc_detail_form(instance=acc_detail)
+        else:
+            form_account_detail=acc_detail_form()
+        form_user_detail=user_detail_form(instance=request.user)
+        form_change_pass=change_password(request.user)
+        form_notif=notifform(instance=request.user)
+
+    context={"form_user":form_user_detail,"form_account":form_account_detail,
+    "form_change_pass":form_change_pass,"form_notif":form_notif}
+    return render(request,"user/account/setting.html",context)
+
+
+class passwordreset(PasswordResetView):
+
+    template_name="user/account/password_reset_form.html"
+    success_url=reverse_lazy("user:reset_password_done")
+    email_template_name='user/account/password_reset_email.html'
+    form_class=emailvalidatepasswordreset
+
+class passwordresetdone(PasswordResetDoneView):
+    template_name="user/account/password_reset_done.html"
+
+class passwordresetconfrim(PasswordResetConfirmView):
+    template_name="user/account/password_reset_confrim.html"
+    success_url=reverse_lazy("user:reset_password_complete")
+
+
+class passwordresetcomplete(PasswordResetCompleteView):
+    template_name="user/account/password_reset_complete.html"
